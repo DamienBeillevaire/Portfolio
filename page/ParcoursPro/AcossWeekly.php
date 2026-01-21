6119734e4953a52e5c7065de8e824e7877ccf524
AC75092726 <damien.beillevaire-externe@acoss.fr>
2026-01-21 11:19:56 +0100

    style

--- page/ParcoursPro/AcossWeekly.php
+++ page/ParcoursPro/AcossWeekly.php
@@ -6,35 +6,280 @@
         <div class="section-title">
             <h3>Résumer de semaine 1</h3>
         </div>
-        <h3>Lundi 12 janvier</h3>
-        -   Présentation de l’entreprise<br>
-            -   Visite du lieu<br>
-            -   Présentation de l’infrastructure des applications<br>
-            -   Mise en place du poste de travail (session)<br>
-            -   Installation de PHP Storm<br>
-
-        <h3>Mardi 13 janvier</h3>
-            -   Mise en place de l’environnement de travail<br>
-            -	Installation de serveur Wamp avec Apache, git et différents outils de travail<br>
-            -	Clone des dépôts de projet Git<br>
-
-        <h3>Mercredi 14 janvier</h3>
-            -	Installation des dépendances pour chaque projet<br>
-            -	Configuration PHP<br>
-            -	Mise en place de tous les prérequis pour faire fonctionner les projets<br>
-
-        <h3>Jeudi 15 janvier</h3>
-            -	Problèmes avec les droits administrateurs sur mon poste<br>
-            -	Après-midi banalisé, rencontre Directeur Générale Urssaf et activité<br>
-
-
-        <h3>Vendredi 16 janvier</h3>
-            -	Début du développement<br>
-            -	Suppression d’un test dans une application via une demande sur ticket<br>
-            -	Exploration du projet<br>
-            -	Prise en main du projet<br>
-            -	Configuration git locale et merge request de mes modifications<br>
-
+        <div class="flex-card">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Lundi 12 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Présentation de l’entreprise</li>
+                    <li class="list-group-item">- Visite du lieu</li>
+                    <li class="list-group-item">- Présentation de l’infrastructure des applications</li>
+                    <li class="list-group-item">- Mise en place du poste de travail (session)</li>
+                    <li class="list-group-item">- Installation de PHP Storm</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mardi 13 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Mise en place de l’environnement de travail</li>
+                    <li class="list-group-item">- Installation de serveur Wamp avec Apache, git et différents outils de travail</li>
+                    <li class="list-group-item">- Clone des dépôts de projet Git</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mercredi 14 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Installation des dépendances pour chaque projet</li>
+                    <li class="list-group-item">- Configuration PHP</li>
+                    <li class="list-group-item">- Mise en place de tous les prérequis pour faire fonctionner les projets</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Jeudi 15 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Problèmes avec les droits administrateurs sur mon poste</li>
+                    <li class="list-group-item">- Après-midi banalisé, rencontre Directeur Générale Urssaf et activité</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Vendredi 16 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Début du développement</li>
+                    <li class="list-group-item">- Suppression d’un test dans une application via une demande sur ticket</li>
+                    <li class="list-group-item">- Exploration du projet</li>
+                    <li class="list-group-item">- Prise en main du projet</li>
+                    <li class="list-group-item">- Configuration git locale et merge request de mes modifications</li>
+                </ul>
+            </div>
+        </div>
+        <div class="section-title">
+            <h3>Résumer de semaine 2</h3>
+        </div>
+        <div class="flex-card">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Lundi 19 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Modification du code de l'application</li>
+                    <li class="list-group-item">- Mise a jour de l'interface utilisateur en interchangeant deux ligne d'un tableau</li>
+                    <li class="list-group-item">- Installation de xDebug sur PHP</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mardi 20 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- Recherche d'une fonction attribuant une données a un attribut d'entité</li>
+                    <li class="list-group-item">- Suppression de cette fonction pour éviter le remplissage de la données contenu dans l'entité</li>
+                    <li class="list-group-item">- Installation de xDebug sur PHP</li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mercredi 21 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Jeudi 22 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Vendredi 23 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+        </div>
+        <div class="section-title">
+            <h3>Résumer de semaine 3</h3>
+        </div>
+        <div class="flex-card">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Lundi 26 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mardi 27 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mercredi 28 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Jeudi 29 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Vendredi 30 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+        </div>
+        <div class="section-title">
+            <h3>Résumer de semaine 4</h3>
+        </div>
+        <div class="flex-card">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Lundi 31 janvier
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mardi 1 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mercredi 2 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Jeudi 3 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Vendredi 4 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+        </div>
+        <div class="section-title">
+            <h3>Résumer de semaine 5</h3>
+        </div>
+        <div class="flex-card">
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Lundi 5 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mardi 6 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Mercredi 7 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Jeudi 8 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <div class="card-header">
+                    Vendredi 9 février
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                    <li class="list-group-item">- </li>
+                </ul>
+            </div>
         </div>
     </section>
 </div>
