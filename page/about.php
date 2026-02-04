43903d8f326f334e7b3931eccad1c2b94a51fc00
AC75092726 <damien.beillevaire-externe@acoss.fr>
2026-02-04 15:40:56 +0100

    ajout des images en ligne

--- page/about.php
+++ page/about.php
@@ -13,7 +13,7 @@
         <p>Vous retrouverez ci-dessous mon CV ainsi que mes différents projets 👩‍💻</p>
         <br>
 
-        <a class="padding-m" href="#" target="_blank" download="CV Damien Beillevaire"><button class="button-74" role="button">CV</button></a>
+        <a class="padding-m" href="" target="_blank" download="CV Damien Beillevaire"><button class="button-74" role="button">CV</button></a>
         <a href="https://www.linkedin.com/in/damien-beillevaire-77983a345" target="_blank"><button class="button-74" role="button">Linkedin</button></a>
 
     </div>
@@ -23,55 +23,61 @@
     </div>
     <div class="flex-card">
         <div class="card" style="width: 8rem;">
-            <img src="https://i.imageupload.app/f622f2559c6010c00e65.png" alt="image">
+            <img src="https://i.imageupload.app/3b7538402d3e33f3bf63.png" alt="HTML">
             <div class="card-body">
                 <p class="card-text">HTML</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompDev/css.png" class="card-img-top" alt="CSS">
+            <img src="https://i.imageupload.app/29bd45ff099e204a4912.png" alt="CSS">
             <div class="card-body">
                 <p class="card-text">CSS</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompDev/js.png" class="card-img-top" alt="JavaScript">
+            <img src="https://i.imageupload.app/d6941fa66a2e80e7a434.png" alt="JavaScript">
             <div class="card-body">
                 <p class="card-text">JavaScript</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompDev/php-logo.png" class="card-img-top" alt="PHP">
+            <img src="https://i.imageupload.app/e7d0bfaf86cf68d20cee.png" alt="PHP">
             <div class="card-body">
                 <p class="card-text">PHP</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompDev/sql.png" class="card-img-top" alt="SQL">
+            <img src="https://i.imageupload.app/6e673f560157ee7a2064.png" class="card-img-top" alt="MySQL">
             <div class="card-body">
-                <p class="card-text">SQL</p>
+                <p class="card-text">MySQL</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="SQL">
+            <img src="https://i.imageupload.app/aa43c3fc779a626f1221.png" class="card-img-top" alt="PostgreSQL">
+            <div class="card-body">
+                <p class="card-text">PostgreSQL</p>
+            </div>
+        </div>
+        <div class="card" style="width: 8rem;">
+            <img src="https://i.imageupload.app/32a871b3ca020ac6f4e3.png" class="card-img-top" alt="TypeScript">
             <div class="card-body">
                 <p class="card-text">TypeScript</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="SQL">
+            <img src="https://i.imageupload.app/17e7d098d8a8ead4509f.png" class="card-img-top" alt="Java">
             <div class="card-body">
                 <p class="card-text">Java</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="SQL">
+            <img src="https://i.imageupload.app/edf4bf7e145b64e92d9a.png" class="card-img-top" alt="Tailwind">
             <div class="card-body">
                 <p class="card-text">Tailwind</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="SQL">
+            <img src="https://i.imageupload.app/fee91a32fbde6a0ab309.png" class="card-img-top" alt="Angular">
             <div class="card-body">
                 <p class="card-text">Angular</p>
             </div>
@@ -84,67 +90,67 @@
     </div>
     <div class="flex-card">
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompLogi/phpstorm.png" class="card-img-top" alt="PHPStorm">
+            <img src="https://i.imageupload.app/7a202265093cfbf330a9.png" alt="PHPStorm">
             <div class="card-body">
                 <p class="card-text">PHP Storm</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompLogi/vsc-logo.png" class="card-img-top" alt="VSC">
+            <img src="https://i.imageupload.app/3b3bce5e1ba8196f06b1.png" alt="VSC">
             <div class="card-body">
                 <p class="card-text">Visual Studio Code</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompLogi/blender.jpg" class="card-img-top" alt="Blender">
+            <img src="https://i.imageupload.app/7e3f4fe64461fc04a7c0.jpeg" alt="Blender">
             <div class="card-body">
                 <p class="card-text">Blender</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="DBeaver">
+            <img src="https://i.imageupload.app/9fa5af2c125b47d4de03.png" class="card-img-top" alt="DBeaver">
             <div class="card-body">
                 <p class="card-text">DBeaver</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="Fork">
+            <img src="https://i.imageupload.app/b7ea365582c5254b57e4.png" class="card-img-top" alt="Fork">
             <div class="card-body">
                 <p class="card-text">Fork</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="Insomnia">
+            <img src="https://i.imageupload.app/7f005a2bcaeb74d2479d.png" class="card-img-top" alt="Insomnia">
             <div class="card-body">
                 <p class="card-text">Insomnia</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="Composer">
+            <img src="https://i.imageupload.app/27845f460e2a1b2810a2.png" class="card-img-top" alt="Composer">
             <div class="card-body">
                 <p class="card-text">Composer</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="GitHub">
+            <img src="https://i.imageupload.app/93899d080b4adc0a2216.png" alt="GitHub">
             <div class="card-body">
                 <p class="card-text">GitHub</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="GitLab">
+            <img src="https://i.imageupload.app/b76ff56bbca8bacd2a52.png" class="card-img-top" alt="GitLab">
             <div class="card-body">
                 <p class="card-text">GitLab</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="Git">
+            <img src="https://i.imageupload.app/5939b505ac9fc9550d9e.png" class="card-img-top" alt="Git">
             <div class="card-body">
                 <p class="card-text">Git</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="#" class="card-img-top" alt="Canva">
+            <img src="https://i.imageupload.app/e05dd44b807805cfbd76.png" class="card-img-top" alt="Canva">
             <div class="card-body">
                 <p class="card-text">Canva</p>
             </div>
@@ -157,13 +163,13 @@
     </div>
     <div class="flex-card">
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompLogi/phpstorm.png" class="card-img-top" alt="PHPStorm">
+            <img src="https://i.imageupload.app/7abe789a3695a7819c17.png" class="card-img-top" alt="Symphony">
             <div class="card-body">
                 <p class="card-text">Symphony</p>
             </div>
         </div>
         <div class="card" style="width: 8rem;">
-            <img src="../www/ressources/img/CompLogi/vsc-logo.png" class="card-img-top" alt="VSC">
+            <img src="https://i.imageupload.app/13ee66a08c732300da07.jpeg" class="card-img-top" alt="React">
             <div class="card-body">
                 <p class="card-text">React</p>
             </div>
--- page/projet.php
+++ page/projet.php
@@ -6,7 +6,7 @@
         <div class="flex-card">
             <div class="card " style="width: 30%;">
                 <div class="card-body">
-                    <img src="../www/ressources/img/Projets/ptfo.png" class="card-img-top" alt="PortFolio">
+                    <img src="https://i.imageupload.app/a16fa454a152f32746e9.png" class="card-img-top" alt="image">
                     <h5 class="card-title">Analyse et maquette portfolio</h5>
                     <p class="card-text">Analyse et maquette portfolio</p>
                     <a class="btn btn-primary" href="../www/ressources/analyse-portefolio-Damien-Beillevaire.pdf" target="_blank" download="Analyse et maquette Damien Beillevaire">Download</a>
