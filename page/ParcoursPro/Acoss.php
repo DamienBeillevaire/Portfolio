6b20a7c78dfdcc3cd3b2ddcad78e7b71da7ffa20
AC75092726 <damien.beillevaire-externe@acoss.fr>
2026-02-10 14:58:18 +0100

    button contact

--- page/ParcoursPro/Acoss.php
+++ page/ParcoursPro/Acoss.php
@@ -3,11 +3,6 @@
         <div class="section-title">
             <h1>Acoss</h1>
         </div>
-        <div class="section-title">
-            <h3>Résumé des semaines</h3>
-        </div>
-        <a href="?route=stageAcossWeekly" class="btn btn-primary">Viens</a>
-        <div>
             <h4>Présentation de l'entreprise</h4>
             <ul>
                 <li>Numéro de SIRET : 180 035 016 00096</li>
@@ -19,6 +14,11 @@
                 un organisme qui procède à la collecte et à la redistribution des cotisations et contributions destinées
                 au financement de la Sécurité sociale.
             </p>
+            <div class="section-title">
+                <h3>Résumé des semaines</h3>
+            </div>
+            <a href="?route=stageAcossWeekly" class="btn btn-primary">Viens</a>
+            <div>
         </div>
     </section>
 </div>
